672fd6c85a64675aa6622dbd9eaea6bad2d77c31
shellysabrina <99389463+shellysabrina@users.noreply.github.com>
2023-11-29 20:02:29 +0700

    murung raya

---
+++ resources/views/login.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Murung Raya</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="login">
+            <form action="">
+                <h1>login</h1>
+                <hr>
+                <p>Kabupaten Murung Raya</p>
+                <label for="">Email</label>
+                <input type="text" placeholder="Enter Email Address...">
+                <label for="">Password</label>
+                <input type="password" placeholder="Password...">
+                <button>Login</button>
+                <p>
+                    <a href="#">Forgot Password</a>
+                </p>
+            </form>
+        </div>
+        <div class="right">
+            <img src="images/lambang.png" alt="">
+        </div>
+    </div>
+    
+</body>
+</html>
